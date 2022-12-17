784461fce5c7128459dc63097f0b749f4f8931dc
rajibrahman74e <rajiburrahman74@gmail.com>
2022-12-18 03:33:35 +0600

    work-almost-finish

--- script.js
+++ script.js
@@ -28,3 +28,35 @@ function subtraction() {
   var subtraction = parseInt(input3) - parseInt(input4);
   subtractionAmmount.innerText = "Your Total ammount is " + subtraction + " BDT!";
 }
+
+
+// MULTIPLICATION
+
+// button selection here
+var multiplicationButton = document.querySelector(".total-button");
+
+function multiplication() {
+  // input selection here
+  var input5 = document.querySelector(".input-05").value;
+  var input6 = document.querySelector(".input-06").value;
+  var multiplicationAmmount = document.querySelector(".multiplication-ammount");
+
+  var multiplication = parseInt(input5) * parseInt(input6);
+  multiplicationAmmount.innerText = "Your Total ammount is " + multiplication + " BDT!";
+}
+
+
+// DIVISION
+
+// button selection here
+var divisionButton = document.querySelector(".division-button");
+
+function division() {
+  // input selection here
+  var input7 = document.querySelector(".input-07").value;
+  var input8 = document.querySelector(".input-08").value;
+  var divisionAmmount = document.querySelector(".division-ammount");
+
+  var division = parseInt(input7) / parseInt(input8);
+  divisionAmmount.innerText = "Your Total ammount is " + division + " BDT!";
+}
