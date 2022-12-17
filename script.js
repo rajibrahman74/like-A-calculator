74f9d203986ad9907b9f88fade1bdaeaf499782b
rajibrahman74e <rajiburrahman74@gmail.com>
2022-12-18 03:22:41 +0600

    subtraction-done

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
+// SUMMATION
+
 // button selection here
 var totalButton = document.querySelector(".total-button");
 
-function num() {
+function summation() {
   // input selection here
   var input1 = document.querySelector(".input-01").value;
   var input2 = document.querySelector(".input-02").value;
@@ -9,4 +11,20 @@ function num() {
 
   var summation = parseInt(input1) + parseInt(input2);
   totalAmmount.innerText = "Your Total ammount is " + summation + " BDT!";
+}
+
+
+// SUBTRACTION
+
+// button selection here
+var totalButton = document.querySelector(".total-button");
+
+function subtraction() {
+  // input selection here
+  var input3 = document.querySelector(".input-03").value;
+  var input4 = document.querySelector(".input-04").value;
+  var subtractionAmmount = document.querySelector(".subtraction-ammount");
+
+  var subtraction = parseInt(input3) - parseInt(input4);
+  subtractionAmmount.innerText = "Your Total ammount is " + subtraction + " BDT!";
 }
